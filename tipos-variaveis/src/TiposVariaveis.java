f782b5d8ba9498bb6f3c0ed00a643dd6157ed729
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-03-04 23:44:47 -0300

    Deixando valor positivo

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,8 +2,13 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         int numero = 5;
 
+        // deixando valor negativo
         numero = -numero;
 
+        System.out.println(numero);
+
+        // deixando valor positivo
+        numero = numero * -1;
         System.out.println(numero);
 
     }
