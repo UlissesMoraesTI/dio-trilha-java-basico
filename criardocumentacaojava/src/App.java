9e65843c714726c0ffc866699353d6808faaca38
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-03-05 11:44:13 -0300

    Criando Documentação do JAVADOC

---
+++ criardocumentacaojava/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/*.java");
+    }
+}
