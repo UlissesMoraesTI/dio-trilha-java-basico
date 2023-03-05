3abbfeb77a1b2efd2295aaaa83de9f23987effeb
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-03-05 10:44:17 -0300

    Documentando Classes e Métodos

--- documentacao-oficial-java/src/App.java
+++
@@ -1,5 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("https://docs.oracle.com/javase/7/docs/api/java/lang/String.html");
-    }
-}
---
+++ documentacao-oficial-java/src/Calculadora.java
@@ -0,0 +1,24 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação destas classes
+ * para entendimento dos recursos oferecidos
+ * 
+ * @author Ulisses Moraes (TI)
+ * @version 1.0
+ * @since 05/03/2023
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * 
+     * @param numero1 este é o primeiro parâmetro do método
+     * @param numero2 este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números
+     * @throws Exception
+     */
+    public int somar(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+}
