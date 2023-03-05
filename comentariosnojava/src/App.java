ef74cf939d6fe8830271a218044e827674ff7fe0
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-03-05 11:10:54 -0300

    Comentários no JAVA

--- comentariosnojava/src/App.java
+++ comentariosnojava/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+        // One Line
+        // comentário de uma linha
+
+        /*
+         * Mult Line
+         * Multiplas linhas de comentário
+         */
+
+        /**
+         * Documentação
+         */
+
+        System.out.println("Aula de Comentários");
         System.out.println("Hello, World!");
     }
 }
