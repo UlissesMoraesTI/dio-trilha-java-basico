5b262710e2649dfd2b8c71ee733dcad16a796135
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-03-04 23:58:07 -0300

    Passar uma boolean de true para false

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,15 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        int numero1 = 5;
-        int numero2 = 9;
+        boolean variavel = true;
+        System.out.println(variavel);
 
-        // incremento
-        numero1++;
-        System.out.println(numero1);
-
-        // decremento
-        numero2--;
-        System.out.println(numero2);
+        variavel = !variavel;
+        System.out.println(variavel);
 
     }
 }
