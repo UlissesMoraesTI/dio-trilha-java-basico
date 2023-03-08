2a4a5be93c7398b6ea4fbdc2a4c8f15a5a97ca73
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-03-08 18:06:07 -0300

    while duas formas

--- capturandonota/src/App.java
+++ capturandonota/src/App.java
@@ -10,6 +10,42 @@ public class App {
         System.out.println("Informe nota do Aluno: ");
         nota = scan.nextInt();
 
+        while (nota < 0 | nota > 10) {
+            System.out.println("Entre com uma nota válida: ");
+            nota = scan.nextInt();
+
+        }
+
+        switch (nota) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("Aluno em recuperação");
+                break;
+            case 5:
+            case 6:
+                System.out.println("Passou raspando!");
+                break;
+            case 7:
+                System.out.println("Boa nota!");
+                break;
+            case 8:
+            case 9:
+                System.out.println("Ótima nota!");
+                break;
+            case 10:
+                System.out.println("Excelente nota!");
+                break;
+            default:
+                System.out.println("Nota Indefinida!");
+                break;
+        }
+
+        System.out.println("Informe nota do Aluno: ");
+        nota = scan.nextInt();
+
         while (true) {
 
             if (nota >= 0 && nota <= 10)
